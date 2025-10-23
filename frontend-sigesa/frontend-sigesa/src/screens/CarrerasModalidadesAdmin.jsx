7034a48f0fc5003a247157b960bbf8c5b69c68cf
Boris Anthony Angulo Urquieta <borisanthony03@gmail.com>
2025-10-23 09:47:25 -0400

    arreglando posicino de archivos

--- frontend-sigesa/frontend-sigesa/src/screens/CarrerasModalidadesAdmin.jsx
+++ frontend-sigesa/frontend-sigesa/src/screens/CarrerasModalidadesAdmin.jsx
@@ -8,7 +8,6 @@ import {
   ChevronDown, 
   ChevronRight, 
   Search,
-  Filter,
   Users,
   CheckCircle,
   Clock,
@@ -24,9 +23,9 @@ import {
   descargarCertificadoCarreraModalidad,
   eliminarCarreraModalidad
 } from '../services/api';
-import ModalSubirCertificado from './ModalSubirCertificado';
-import PlameModal from './PlameModal';
-import './CarrerasModalidadesAdmin.css';
+import ModalSubirCertificado from '../components/ModalSubirCertificado';
+import PlameModal from '../components/PlameModal';
+import '../styles/CarrerasModalidadesAdmin.css';
 
 const CarrerasModalidadesAdmin = () => {
   const navigate = useNavigate();
@@ -395,7 +394,6 @@ const CarrerasModalidadesAdmin = () => {
 
         <div className="filters-row">
           <div className="filter-group">
-            <Filter size={16} />
             <select
               value={selectedFacultad}
               onChange={(e) => setSelectedFacultad(e.target.value)}
